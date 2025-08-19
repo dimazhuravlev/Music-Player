bb48fe895233af1599db35137b3f042c5a5e14a6
dimazhuravlev <dmitryzhur@gmail.com>
2025-08-19 21:20:52 +0200

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Dmitrii Zhuravlev on 19.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
